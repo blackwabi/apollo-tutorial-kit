c3c5b3510d5c08c445b392f590d688b9b86eaf48
meganho <bbeeaanns@gmail.com>
2017-08-25 16:19:59 +0200

    Defining a schema

--- data/schema.js
+++ data/schema.js
@@ -1,17 +1,23 @@
-import {
-  makeExecutableSchema,
-  addMockFunctionsToSchema,
-} from 'graphql-tools';
-import mocks from './mocks'
+import { makeExecutableSchema } from 'graphql-tools';
 
 const typeDefs = `
+type Author {
+  id: Int
+  firstName: String
+  lastName: String
+  posts: [Post]
+}
+type Post {
+  id: Int
+  title: String
+  text: String
+  views: Int
+  author: Author
+}
 type Query {
-  testString: String
+  author(firstName: String, lastName: String): Author
+  getFortuneCookie: String
 }
 `;
 
-const schema = makeExecutableSchema({ typeDefs });
-
-addMockFunctionsToSchema({ schema, mocks });
-
-export default schema;
+export default makeExecutableSchema({ typeDefs });
